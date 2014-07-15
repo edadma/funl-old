2f542deaee803090d054abb8235778d3ce67bcaf
Edward A. Maxedon, Sr <edadma@gmail.com>
2014-07-15 17:25:17 -0400

    added module 'time' and rosettacode.org/wiki/Terminal_control/Hiding_the_cursor#FunL task

--- src/main/scala/modules/IO.scala
+++ src/main/scala/modules/IO.scala
@@ -1,9 +1,9 @@
-/*     ______            __                                      *\
-**    / ____/_  __ ___  / /     FunL Programming Language        **
-**   / __/ / / / / __ \/ /      (c) 2014, Edward A. Maxedon, Sr. **
-**  / /   / /_/ / / / / /__     http://funl-lang.org/            **
-** /_/    \____/_/ /_/____/                                      **
-\*                                                               */
+/*     ______            __                                     *\
+**    / ____/_  __ ___  / /     FunL Programming Language       **
+**   / __/ / / / / __ \/ /      (c) 2014 Edward A. Maxedon, Sr. **
+**  / /   / /_/ / / / / /__     http://funl-lang.org/           **
+** /_/    \____/_/ /_/____/                                     **
+\*                                                              */
 
 package funl.modules
 
@@ -11,13 +11,13 @@ import java.io.{File, RandomAccessFile}
 
 object IO
 {
-	def readFile( a: List[Any] ) =
+	def readFile( a: Any ) =
 	{
 		val file =
 			a match
 			{
-				case List( f: File ) => new RandomAccessFile( f, "r" )
-				case List( f: String ) => new RandomAccessFile( f, "r" )
+				case f: File => new RandomAccessFile( f, "r" )
+				case f: String => new RandomAccessFile( f, "r" )
 			}
 		val data = new Array[Byte](file.length.toInt)
 		
