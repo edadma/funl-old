f1c85d2dec73ba7e024b87c55f7e4670086ab5e3
Edward A. Maxedon, Sr <edadma@gmail.com>
2014-07-19 19:33:05 -0400

    added rosettacode.org/wiki/Function_composition#FunL task and added to tests

--- src/test/scala/RosettaCode.scala
+++ src/test/scala/RosettaCode.scala
@@ -73,4 +73,5 @@ class RosettaCode extends FreeSpec with PropertyChecks with Matchers
 	"Modular_inverse" in {executeCaptureOutput( "RosettaCode.org/Modular_inverse", Some("-main-") ).trim shouldBe "1969"}
   "Topological_sort" in {executeCaptureOutput( "RosettaCode.org/Topological_sort", Some("-main-") ).trim shouldBe
     "[synopsys, ieee, std, dware, std_cell_lib, gtech, ramlib, dw06, dw05, dw02, dw07, dw01, dw03, dw04, des_system_lib]"}
+	"Function_composition" in {executeCaptureOutput( "RosettaCode.org/Function_composition", Some("-main-") ).trim shouldBe "0.5"}
 }
