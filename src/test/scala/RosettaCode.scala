8b1e70f73d89df0dde3541b20133b085fab218bd
Edward A. Maxedon, Sr <edadma@gmail.com>
2014-07-25 22:29:41 -0400

    fixed tests

--- src/test/scala/RosettaCode.scala
+++ src/test/scala/RosettaCode.scala
@@ -82,9 +82,9 @@ class RosettaCode extends FreeSpec with PropertyChecks with Matchers
 	"JSON" in {executeCaptureOutput( "RosettaCode.org/JSON", Some("-main-") ).trim shouldBe 
 		"""	|{"foo": 1, "bar": [10, "apples"]}
 				|{
-				|  "foo": 1.0,
+				|  "foo": 1,
 				|  "bar": [
-				|    10.0,
+				|    10,
 				|    "apples"
 				|  ]
 				|}""".stripMargin}
