585a512c1249fd919cb2dc6684dd6b829103531d
Edward A Maxedon Sr <edadma@gmail.com>
2015-07-14 18:28:00 -0400

    generalize REPL

--- src/main/scala/REPL.scala
+++ src/main/scala/REPL.scala
@@ -17,73 +17,80 @@ import funl.interp.Interpreter._
 
 object REPL extends App
 {
-	System.getProperties.setProperty( "jline.shutdownhook", "true" )
+	val welcome =
+		"""	|    ______            __
+			|   / ____/_  __ ___  / /     FunL Programming Language
+			|  / __/ / / / / __ \/ /      (c) 2014 Edward A. Maxedon, Sr.
+			| / /   / /_/ / / / / /__     http://funl-lang.org/
+			|/_/    \____/_/ /_/____/
+			|                                                             """.stripMargin.lines ++
+		Iterator (
+			"Welcome to FunL version " + VERSION,
+			"Type in expressions to have them evaluated.",
+			"Type :help for more information."
+		)
 
-	val reader = new ConsoleReader
-	val out = new PrintWriter( reader.getTerminal.wrapOutIfNeeded(System.out), true )
-	var line: String = null
-	val eval = new Evaluator
-	var count = 1
-	implicit val env = new eval.Environment
-
-	reader.setBellEnabled( false )
-	reader.setPrompt( "> " )
-
-	for {line <- """|    ______            __
-									|   / ____/_  __ ___  / /     FunL Programming Language
-									|  / __/ / / / / __ \/ /      (c) 2014 Edward A. Maxedon, Sr.
-									| / /   / /_/ / / / / /__     http://funl-lang.org/
-									|/_/    \____/_/ /_/____/                                     
-									|                                                             """.stripMargin.lines}
-		out.println( line )
-
-	out.println( "Welcome to FunL version " + VERSION )
-	out.println( "Type in expressions to have them evaluated." )
-	out.println( "Type :help for more information." )
-	out.println
+	start( welcome )
 	
-	eval.loadPredef( "REPL" )
-	eval.enterActivation( null, null, eval.module("REPL") )
-	
-	while ({line = reader.readLine; line != null})
-	{
-		line.trim match
+	def start( welcome: Iterator[String] ) {
+		System.getProperties.setProperty( "jline.shutdownhook", "true" )
+
+		val reader = new ConsoleReader
+		val out = new PrintWriter( reader.getTerminal.wrapOutIfNeeded(System.out), true )
+		var line: String = null
+		val eval = new Evaluator
+		var count = 1
+		implicit val env = new eval.Environment
+
+		for (l <- welcome)
+			out.println( l )
+			
+		out.println
+		reader.setBellEnabled( false )
+		reader.setPrompt( "> " )
+		eval.loadPredef( "REPL" )
+		eval.enterActivation( null, null, eval.module("REPL") )
+		
+		while ({line = reader.readLine; line != null})
 		{
-			case ":help" =>
-				out.println( ":help                      print this summary" )
-				out.println( ":quit                      exit the REPL" )
-				out.println
-			case ":quit" =>
-				sys.exit
-			case "" =>
-				out.println
-			case _ =>
-				try
-				{
-					statement( "REPL", line, eval ) match
-					{
-						case None =>
-						case Some( res ) =>
-						val name = "res" + count
-
-							if (res == null)
-								out.println( name + " = null" )
-							else
-								out.println( name + ": " + res.getClass.getName + " = " + display(res) )
-								
-							eval.assign( "REPL", name -> ConstantReference(name, res) )
-							count += 1
-					}
-					
+			line.trim match
+			{
+				case ":help" =>
+					out.println( ":help                      print this summary" )
+					out.println( ":quit                      exit the REPL" )
 					out.println
-				}
-				catch
-				{
-					case e: Exception =>
-//					e.printStackTrace( out )
-						out.println( e )
+				case ":quit" =>
+					sys.exit
+				case "" =>
+					out.println
+				case _ =>
+					try
+					{
+						statement( "REPL", line, eval ) match
+						{
+							case None =>
+							case Some( res ) =>
+							val name = "res" + count
+
+								if (res == null)
+									out.println( name + " = null" )
+								else
+									out.println( name + ": " + res.getClass.getName + " = " + display(res) )
+									
+								eval.assign( "REPL", name -> ConstantReference(name, res) )
+								count += 1
+						}
+						
 						out.println
-				}
+					}
+					catch
+					{
+						case e: Exception =>
+	//					e.printStackTrace( out )
+							out.println( e )
+							out.println
+					}
+			}
 		}
 	}
 }
